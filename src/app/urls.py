c859859e79d5bf1ba9bdac9a8a55d1278f62fc49
David <davidforastier10@gmail.com>
2023-10-18 10:16:29 -0300

    Registro de usuario

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import home
 from django.contrib.auth import views as auth_views
 
@@ -7,5 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.logout_then_login, name='logout')
+    path('logout/', auth_views.logout_then_login, name='logout'),
+
+
+    #Includes de usuarios
+    path('usuario/', include('usuarios.urls'))
 ]
--- src/usuarios/views.py
+++ src/usuarios/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Inicia sesión automáticamente después del registro
+            return redirect('home')  # Redirige a la página de inicio o a cualquier otra página deseada
+    else:
+        form = UserRegisterForm()
+    return render(request, 'usuarios/register.html', {'form': form})
