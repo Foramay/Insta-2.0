133f008597c39048ee490ed4fc5ec53f4fdd9136
David <davidforastier10@gmail.com>
2024-01-03 10:13:00 -0300

    Busqueda de dos canales de usuarios

--- src/dm/models.py
+++ src/dm/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import uuid
 from usuarios.models import Usuario
-
+from django.db.models import Count
 
 class ModelBase(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, db_index=True, editable=False)
@@ -20,6 +20,14 @@ class CanalMensaje(ModelBase):
 class CanalUsuario(ModelBase):
     canal = models.ForeignKey("Canal", null=True, on_delete=models.SET_NULL)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+class CanalQuerySet(models.QuerySet):
+    def solo_dos(self):
+        return self.annotate(num_usuarios=Count("usuarios")).filter(num_usuarios=2)
+
+class CanalManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return CanalQuerySet(self.model, using=self._db)
 
 class Canal(ModelBase):
     usuarios = models.ManyToManyField(Usuario, blank=True, through=CanalUsuario)
