427527795bc51ace90a7c49d0b129d80bf27d527
David <davidforastier10@gmail.com>
2024-01-16 15:50:13 -0300

    El modelo de canal en el administrador

--- src/dm/admin.py
+++ src/dm/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import CanalMensaje, Canal, CanalUsuario
 
-# Register your models here.
+
+class CanalMensajeInline(admin.TabularInline):
+    model = CanalMensaje
+    extra = 1
+
+class CanalUsuarioInline(admin.TabularInline):
+    model = CanalUsuario
+    extra = 1
+
+class CanalAdmin(admin.ModelAdmin):
+    inlines = [CanalMensajeInline, CanalUsuarioInline]
+
+    class Meta:
+        model = Canal
+
+admin.site.register(Canal, CanalAdmin)
+admin.site.register(CanalMensaje)
+admin.site.register(CanalUsuario)
